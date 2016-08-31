cc53f3ac9fc852af8138056e2bc3d2ffc2b38935
Seth Axen <seth.axen@gmail.com>
2016-08-31 01:45:41 -0700

    removed scipy as import

--- e3fp_paper/crossvalidation/kernels.pyx
+++ e3fp_paper/crossvalidation/kernels.pyx
@@ -7,7 +7,6 @@ import cython
 cimport cython
 import numpy as np
 cimport numpy as np
-import scipy as sc
 
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
@@ -36,11 +35,11 @@ def tanimoto_kernel(X, Y):
           chemical informatics." Neural Networks. 2005. 18(8): 1093-1110.
           doi: 10.1.1.92.483
     """
-    if isinstance(X, sc.sparse.csr_matrix):
+    try:
         return tanimoto_kernel_sparse_(X.indptr, X.indices, X.shape[0],
                                        Y.indptr, Y.indices, Y.shape[0],
                                        X.shape[1])
-    else:
+    except AttributeError:
         return tanimoto_kernel_dense_(np.asarray(X, dtype=DTYPE),
                                       np.asarray(Y, dtype=DTYPE))
 
