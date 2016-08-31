4345cf1c1b81d27dee22752cf0870ed6ba07f98b
Seth Axen <seth.axen@gmail.com>
2016-08-31 11:19:27 -0700

    Revert "reimplemented Tanimoto kernel in Cython"
    This reverts commit 43e933610916ea9204becc0df63581c98ef2834b.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    name="E3FP paper code",
-    ext_modules=cythonize('e3fp_paper/crossvalidation/kernels.pyx'),
-    include_dirs=[numpy.get_include()]
-)
