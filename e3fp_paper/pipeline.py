ac5efb916d6f6f3d9a57f2947fc335bf79297305
Seth Axen <seth.axen@gmail.com>
2016-09-20 16:09:14 -0700

    added methods for creating 2D fingerprints.

--- e3fp_paper/pipeline.py
+++ e3fp_paper/pipeline.py
@@ -5,9 +5,11 @@ E-mail: seth.axen@gmail.com
 """
 import os
 
+from rdkit import Chem
 from fpcore.fconvert import string2ascii, ascii2string
 
 from e3fp.config.params import read_params
+from e3fp.conformer.util import mol_from_sdf
 from e3fp.fingerprint.fprint import Fingerprint
 from e3fp.pipeline import fprints_from_smiles, fprints_from_sdf, \
                           fprints_from_mol
@@ -17,13 +19,16 @@ CONFIG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 PARAM_FILE = os.path.join(CONFIG_DIR, "best_params.cfg")
 FIT_FILE = os.path.join(CONFIG_DIR, "best_params.fit")
 
+
 def load_params():
     """Load best parameter set from file."""
     return read_params(PARAM_FILE)
 
+
 def get_fit_file():
     """Get path to SEA library fit file for best parameter set."""
     return FIT_FILE
+
 
 def fprint_to_native_tuple(fprint):
     """Convert fingerprint to tuple with native string, name."""
@@ -42,6 +47,7 @@ def native_tuple_to_fprint(native_tuple):
 
 
 def native_tuples_from_mol(mol, fprint_params={}, save=False):
+    """Fingerprint molecule and convert to native encoding."""
     if not mol.HasProp("_Name"):
         raise ValueError(
             "mol must have a '_Name' property or `name` must be provided")
@@ -68,3 +74,18 @@ def native_tuples_from_sdf(sdf_file, fprint_params={}, save=False):
                                     save=save)
     native_tuples = list(map(fprint_to_native_tuple, fprints_list))
     return native_tuples
+
+
+def fprint2d_from_mol(mol, bits=1024, radius=2):
+    """Generate ECFP fingerprint from mol."""
+    bitvect = Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius,
+                                                                  bits)
+    fp = Fingerprint.from_rdkit(bitvect)
+    fp.name = mol.GetProp("_Name")
+    return fp
+
+
+def fprint2d_from_sdf(sdf_file, bits=1024, radius=2):
+    """Generate ECFP fingerprint from sdf file."""
+    mol = mol_from_sdf(sdf_file)
+    return fprint2d_from_mol(mol, bits=bits, radius=radius)
