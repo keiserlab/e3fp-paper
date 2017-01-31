3bbf91170bfcdadd5f5270c6e49573109b4f118b
Seth Axen <seth.axen@gmail.com>
2017-01-31 10:45:45 -0800

    Add PRC plotting features

--- e3fp_paper/plotting/validation.py
+++ e3fp_paper/plotting/validation.py
@@ -96,36 +96,79 @@ def plot_roc_curves(roc_lists, ax, y_min=0., names=None, colors=None,
                   fontsize=fonts.legend_fontsize)
 
 
-def plot_prc_curves(rec_prec_tuples_list, ax, names=None, show_legend=False,
-                    title="", ref_val=None, colors=None, alpha=1.):
-    """Plot precision-recall (PRC) curve(s)."""
-    if names is None or len(names) != len(rec_prec_tuples_list):
-        names = ["" for i in xrange(len(rec_prec_tuples_list))]
+def plot_prc_curves(prc_lists, ax, names=None, colors=None, ref_val=None,
+                    show_legend=True, only_best=True, title="", alpha=1.):
+    """Plot precision-recall (PRC) curves
 
-    lines = []
-    for i, rec_prec_tuple in enumerate(rec_prec_tuples_list):
-        try:
-            color = colors[i]
-        except IndexError:
-            color = colors
-        line, = ax.plot(*rec_prec_tuple, linewidth=2, label=names[i],
-                        zorder=i + 2, color=color, alpha=alpha)
-        lines.append(line)
+    Parameters
+    ----------
+    prc_lists : list of ndarray or list of list of ndarray
+        If list of ndarray, each array corresponds to a single PRC curve, where
+        the first and second indices of the array correspond to the FPR and TPR,
+        respectively. If list of list of ndarray, then each list of ndarray is
+        considered to be a set of PRC curves that should be grouped. 
+    ax : axis
+        matplotlib axis
+    y_min : float, optional
+        Minimum value of y, used to visually discriminate between well-
+        performing curves.
+    names : list of str or None, optional
+        Names of PRC sets in `prc_lists`
+    colors : list or None, optional
+        Colors to be used for PRC sets in `prc_lists`
+    ref_val : float or None, optional
+        If float is provided, value corresponds to the PRC expected from a
+        random classifier, which is a horizontal line corresponding to percent
+        of pairs which are positives.
+    show_legend : bool, optional
+        Show legend.
+    only_best : bool, optional
+        Only plot best (highest AUC) PRC curve in a set.
+    title : str, optional
+        Title of plot
+    alpha : float, optional
+        Alpha of curves.
+    """
+    legend_lines = []
+    legend_names = []
 
-    names = names[::-1]
-    lines = lines[::-1]
+    if isinstance(prc_lists[0], np.ndarray):
+        prc_list = [[x] for x in prc_list]
+
+    for i, prc_list in enumerate(prc_lists):
+        color = get_from_list(colors, i)
+        name = get_from_list(names, i)
+        aucs = [get_auc(x[0], x[1]) for x in prc_list]
+        auc_prcs = sorted(zip(aucs, prc_list), reverse=True)
+        auc = auc_prcs[0][0]
+        if only_best:
+            line, = ax.plot(auc_prcs[0][1][0], auc_prcs[0][1][1], linewidth=1,
+                            zorder=i + 2, color=color, alpha=alpha)
+            name += " ({:.4f})".format(auc)
+        else:
+            for j, (auc, prc) in enumerate(auc_prcs):
+                line, = ax.plot(prc[0], prc[1], linewidth=1,
+                                zorder=i + 2, color=color, alpha=alpha)
+            name += " ({:.4f} +/- {:.4f})".format(np.mean(zip(*auc_prcs)[0]),
+                                                  np.std(zip(*auc_prcs)[0]))
+        legend_lines.append(line)
+        legend_names.append(name)
+
+    legend_lines = legend_lines[::-1]
+    legend_names = legend_names[::-1]
     if ref_val is not None:
         line, = ax.plot([0, 1], [ref_val, ref_val], linewidth=1,
                         color="lightgrey", linestyle="--", label="Random",
                         zorder=1)
-        lines.append(line)
-        names = names + ["Random"]
+        legend_lines.append(line)
+        legend_names.append("Random ({:.4f})".format(ref_val))
 
     ax.set_xlim(0., 1.01)
-    ax.set_ylim(0., 1.01)
+    ax.set_ylim(0, 1.001)
     ax.set_aspect(1. / ax.get_data_ratio())
     ax.set_xlabel("Recall", fontsize=fonts.ax_label_fontsize)
     ax.set_ylabel("Precision", fontsize=fonts.ax_label_fontsize)
     ax.set_title(title, fontsize=fonts.title_fontsize)
     if show_legend:
-        ax.legend(lines, names, loc=3, fontsize=fonts.legend_fontsize)
+        ax.legend(legend_lines, legend_names, loc=3,
+                  fontsize=fonts.legend_fontsize)
