d359d34f41b3eb6b0294f4fc7330ee439d355050
Seth Axen <seth.axen@gmail.com>
2017-05-06 21:24:13 -0700

    Allow offset to be specified

--- e3fp_paper/plotting/util.py
+++ e3fp_paper/plotting/util.py
@@ -7,8 +7,9 @@ from .defaults import DefaultFonts
 
 fonts = DefaultFonts()
 
-def add_panel_label(label, ax):
+
+def add_panel_label(label, ax, xoffset=-.2, yoffset=1.12):
     """Add a panel label to an axis."""
-    ax.text(-.2, 1.12, label, transform=ax.transAxes,
+    ax.text(xoffset, yoffset, label, transform=ax.transAxes,
             fontsize=fonts.panel_label_fontsize, fontweight='bold', va='top',
-            ha='right')
+            ha='left')
