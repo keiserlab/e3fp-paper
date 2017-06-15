92bf7542eb3b28b567d90194a1de5369ecf572a1
Seth Axen <seth.axen@gmail.com>
2017-06-14 17:20:55 -0700

    Save to binary NumPy memmap
    Saving to binary NumPy memmap keeps the memory usage at a minimum and
    allows future use of memmap without heavy memory requirements.

--- project/fingerprint_comparison/binary_to_numpy.py
+++ project/fingerprint_comparison/binary_to_numpy.py
@@ -1,4 +1,4 @@
-"""Concatenate binary lower triangular matrix data into numpy array.
+"""Concatenate binary lower triangular matrix data into numpy memmap.
 
 Author: Seth Axen
 E-mail: seth.axen@gmail.com
@@ -18,30 +18,44 @@ def start_end_indices_from_fn(fn):
     return start_index, end_index
 
 
-def main(bin_files, mol_name_files, np_file):
-    data_list = []
+def main(bin_files, mol_name_files, np_file, out_mol_names_file):
     max_index = 0
     bin_files = sorted(bin_files, key=start_end_indices_from_fn)
     mol_name_files = sorted(mol_name_files, key=start_end_indices_from_fn)
+
+    _, max_index = start_end_indices_from_fn(mol_name_files[-1])
+    total_expect_size = get_batch_size(0, max_index)
+    print("Instantiating memmap of length {} at {}.".format(
+        total_expect_size, np_file))
+    memmap = np.memmap(np_file, mode="w+", dtype=np.double,
+                       shape=(total_expect_size,))
+
+    data_count = 0
     for bin_file, mol_name_file in zip(bin_files, mol_name_files):
         start_ind, end_ind = start_end_indices_from_fn(bin_file)
         expect_size = get_batch_size(start_ind, end_ind)
-        max_index = max(max_index, end_ind)
         print("Reading from {}".format(bin_file))
         with smart_open(bin_file, "rb") as f:
-            this_data_list = array.array('d', f.read()).tolist()
-            diff = len(this_data_list) - expect_size
+            data_list = array.array('d', f.read()).tolist()
+            diff = len(data_list) - expect_size
             if diff > 0:
                 if diff == end_ind:  # handle old +1 error
                     print("Trimming redundant row from file.")
-                    this_data_list = this_data_list[:-diff]
+                    data_list = data_list[:-diff]
                 else:
                     sys.exit(
                         "Unexpected size difference in file: {}.".format(diff))
-            data_list.extend(this_data_list)
+
+            print("Adding values to memmap.")
+            memmap[data_count:data_count + len(data_list)] = data_list
+            data_count += len(data_list)
+
+    if data_count != total_expect_size:
+        sys.exit("Total entry number {} deviates from expected {}.".format(
+            data_count, total_expect_size))
 
     mol_names_list = []
-    print("Reading mol names from {}".format(mol_name_file))
+    print("Reading mol names from {}.".format(mol_name_file))
     with smart_open(mol_name_file, "rb") as f:
         for line in f.readlines():
             mol_names_list.append(line.rstrip())
@@ -49,26 +63,22 @@ def main(bin_files, mol_name_files, np_file):
         sys.exit("Number of mol names {} doesn't match data {}.".format(
             len(mol_names_list), max_index + 1))
 
-    total_expect_size = get_batch_size(0, max_index)
-    if len(data_list) != total_expect_size:
-        sys.exit("Total entry number {} deviates from expected {}.".format(
-            len(data_list), total_expect_size))
-
-    print("Converting to array and saving.")
-    data = np.array(data_list, dtype=np.double)
-    np.savez_compressed(np_file, data, np.asarray(mol_names_list))
+    print("Saving mol names to {}.".format(out_mol_names_file))
+    with smart_open(out_mol_names_file, "w") as f:
+        f.write("\n".join(mol_names_list) + "\n")
 
 
 if __name__ == "__main__":
     usage = ("python binary_to_numpy.py <binary_matrix_files> "
-             "<mol_name_files> <numpy_array_file>")
+             "<mol_name_files> <numpy_array_file> <mol_names_file>")
     try:
-        in_files = sys.argv[1:-1]
+        in_files = sys.argv[1:-2]
         bin_files = in_files[:len(in_files) / 2]
         mol_name_files = in_files[-len(in_files) / 2:]
-        np_file = sys.argv[-1]
+        np_file = sys.argv[-2]
+        mol_names_file = sys.argv[-1]
     except:
         sys.exit(usage)
     if len(bin_files) < 1 or len(mol_name_files) != len(bin_files):
         sys.exit(usage)
-    main(bin_files, mol_name_files, np_file)
+    main(bin_files, mol_name_files, np_file, mol_names_file)
