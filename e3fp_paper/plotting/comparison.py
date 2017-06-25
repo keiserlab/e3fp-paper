0e08922a67c4fe80312c6169e9b2c8166b2e6731
Seth Axen <seth.axen@gmail.com>
2017-06-24 20:24:08 -0700

    Move TC plotting methods to package

---
+++ e3fp_paper/plotting/comparison.py
@@ -0,0 +1,114 @@
+"""Methods for plotting fingerprint comparisons.
+
+Author: Seth Axen
+E-mail: seth.axen@gmail.com
+"""
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib
+import seaborn as sns
+from e3fp_paper.plotting.defaults import DefaultFonts
+
+
+fonts = DefaultFonts()
+
+
+def calculate_line(x, m=1., b=0.):
+    return m * x + b
+
+
+def get_weighted_leastsq(xdata, ydata, weights=None):
+    if weights is None:
+        sigma = None
+        weights = np.ones_like(xdata)
+    else:
+        sigma = 1. / np.sqrt(weights)
+    popt, pcov = curve_fit(calculate_line, xdata, ydata, [1., 0.], sigma=sigma)
+    residuals = ydata - calculate_line(xdata, *popt)
+    resid_sumsq = np.dot(weights, residuals**2)
+    mean_y = np.average(ydata, weights=weights)
+    total_sumsq = np.dot(weights, (ydata - mean_y)**2)
+    r2 = 1. - (resid_sumsq / total_sumsq)
+    return popt[0], popt[1], r2
+
+
+def plot_tc_scatter(outlier_df, ax):
+    ul = outlier_df.loc["Upper left"]
+    ur = outlier_df.loc["Upper"]
+    lr = outlier_df.loc["Lower Right"]
+    fr = outlier_df.loc["Far Right"]
+    for df, ms in zip([ul, ur, lr, fr], ['X', 'P', 's', 'D']):
+        ax.scatter(df["ECFP4 TC"], df["E3FP TC"], s=20, linewidths=1,
+                   marker=ms, facecolors='r', edgecolors='none', alpha=.8,
+                   zorder=100)
+
+
+def plot_tc_heatmap(counts_df, ax, outliers_df=None, cols=[], title="",
+                    ref_line=True, thresholds=[], cmap="bone_r", limit=.5):
+    x, y, count = counts_df[cols[0]], counts_df[cols[1]], counts_df["Count"]
+
+    if ref_line:
+        ax.plot([0, 1], [0, 1], linewidth=1, color="gray", alpha=.5,
+                linestyle="--", label="Equal", zorder=2)
+        if len(thresholds) > 0:
+            xthresh, ythresh = thresholds
+            ax.axvline(xthresh, linewidth=1, color="royalblue", alpha=.75,
+                       linestyle='--', zorder=2)
+            ax.axhline(ythresh, linewidth=1, color="forestgreen",
+                       alpha=.75, linestyle='--', zorder=2)
+        slope, intercept, r2 = get_weighted_leastsq(x, y, weights=count)
+        ax.plot([0, 1], [intercept, slope + intercept], linewidth=1,
+                color="r", alpha=.75, linestyle="--", label="Trend", zorder=2)
+        print("Fit with slope {:.4f}, intercept {:.4f}, and R^2 {:.4f}".format(
+            slope, intercept, r2))
+
+    max_val = max(max(x), max(y), limit)
+    extent = (0, max_val, 0, max_val)
+
+    ax.hexbin(x, y, C=count, cmap=cmap, norm=matplotlib.colors.LogNorm(),
+              gridsize=50, extent=extent, edgecolors='none', zorder=1)
+
+    if outliers_df is not None:
+        x, y = outliers_df[cols[0]], outliers_df[cols[1]]
+        if len(x) > 0:
+            ax.scatter(x, y, s=15, marker="x", facecolors="r",
+                       edgecolors='none', alpha=.3, zorder=3)
+
+    ax.set_xlim(0., max_val + .02)
+    ax.set_ylim(0., max_val + .02)
+    ax.set_aspect('equal')
+    ax.set_xlabel(cols[0], fontsize=fonts.ax_label_fontsize)
+    ax.set_ylabel(cols[1], fontsize=fonts.ax_label_fontsize)
+    ax.set_title(title, fontsize=fonts.title_fontsize)
+
+
+def plot_tc_hists(counts_df, ax, cols=[], title="", colors=[], thresholds=[]):
+    ax.set_yscale("log")
+    count = counts_df["Count"]
+    ref_line_colors = ["royalblue", "forestgreen"]
+    for i, col in enumerate(cols):
+        name = col.split(" ")[0]
+        tcs = counts_df[col]
+        alpha = 1.
+        if i > 0:
+            alpha = .9
+        else:
+            alpha = 1.
+        try:
+            color = colors[i]
+        except IndexError:
+            color = None
+        sns.distplot(tcs, label=name, kde=False, ax=ax, norm_hist=True,
+                     color=color, hist_kws={"linewidth": 0,
+                                            "histtype": "stepfilled",
+                                            "alpha": alpha,
+                                            "zorder": 2 * i + 1,
+                                            "weights": count})
+        ax.axvline(thresholds[i], linewidth=1, color=ref_line_colors[i],
+                   alpha=.75, linestyle='--', zorder=2 * i + 2)
+
+    ax.set_xlabel("TC", fontsize=fonts.ax_label_fontsize)
+    ax.set_ylabel("Log Frequency", fontsize=fonts.ax_label_fontsize)
+    ax.set_xlim(0, counts_df[cols].values.max())
+    ax.set_title(title, fontsize=fonts.title_fontsize)
+    ax.legend(loc=1, fontsize=fonts.legend_fontsize)
--- project/analysis/comparison/make_plots.py
+++ project/analysis/comparison/make_plots.py
@@ -8,9 +8,7 @@ import glob
 import logging
 
 import numpy as np
-from scipy.optimize import curve_fit
 import pandas as pd
-import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib import gridspec
 import seaborn as sns
@@ -20,6 +18,8 @@ from e3fp_paper.plotting.defaults import DefaultColors, DefaultFonts
 from e3fp_paper.plotting.util import add_panel_labels
 from e3fp_paper.plotting.validation import plot_roc_curves, plot_prc_curves, \
                                            plot_auc_stats, plot_auc_scatter
+from e3fp_paper.plotting.comparison import plot_tc_scatter, plot_tc_hists, \
+                                           plot_tc_heatmap
 from e3fp_paper.crossvalidation.util import rocs_from_cv_dir, \
                                             prcs_from_cv_dir, \
                                             prc_roc_aucs_from_cv_dirs, \
@@ -53,105 +53,6 @@ FRAC_POS = .0051
 ECFP_TC_THRESH = .3
 E3FP_TC_THRESH = .2
 setup_logging(verbose=True)
-
-
-def plot_tc_scatter(outlier_df, ax):
-    ul = outlier_df.loc["Upper left"]
-    ur = outlier_df.loc["Upper"]
-    lr = outlier_df.loc["Lower Right"]
-    fr = outlier_df.loc["Far Right"]
-    for df, ms in zip([ul, ur, lr, fr], ['X', 'P', 's', 'D']):
-        ax.scatter(df["ECFP4 TC"], df["E3FP TC"], s=20, linewidths=1,
-                   marker=ms, facecolors='r', edgecolors='none', alpha=.8,
-                   zorder=100)
-
-
-def calculate_line(x, m=1., b=0.):
-    return m * x + b
-
-
-def get_weighted_leastsq(xdata, ydata, weights=None):
-    if weights is None:
-        sigma = None
-        weights = np.ones_like(xdata)
-    else:
-        sigma = 1. / np.sqrt(weights)
-    popt, pcov = curve_fit(calculate_line, xdata, ydata, [1., 0.], sigma=sigma)
-    residuals = ydata - calculate_line(xdata, *popt)
-    resid_sumsq = np.dot(weights, residuals**2)
-    mean_y = np.average(ydata, weights=weights)
-    total_sumsq = np.dot(weights, (ydata - mean_y)**2)
-    r2 = 1. - (resid_sumsq / total_sumsq)
-    return popt[0], popt[1], r2
-
-
-def plot_tc_heatmap(counts_df, ax, outliers_df=None, cols=[], title="",
-                    ref_line=True, cutoffs=[], cmap="bone_r", limit=.5):
-    x, y, count = counts_df[cols[0]], counts_df[cols[1]], counts_df["Count"]
-
-    if ref_line:
-        ax.plot([0, 1], [0, 1], linewidth=1, color="gray", alpha=.5,
-                linestyle="--", label="Equal", zorder=2)
-        ax.axhline(E3FP_TC_THRESH, linewidth=1, color="forestgreen",
-                   alpha=.75, linestyle='--', zorder=2)
-        ax.axvline(ECFP_TC_THRESH, linewidth=1, color="royalblue", alpha=.75,
-                   linestyle='--', zorder=2)
-        slope, intercept, r2 = get_weighted_leastsq(x, y, weights=count)
-        ax.plot([0, 1], [intercept, slope + intercept], linewidth=1,
-                color="r", alpha=.75, linestyle="--", label="Trend", zorder=2)
-        print("Fit with slope {:.4f}, intercept {:.4f}, and R^2 {:.4f}".format(
-            slope, intercept, r2))
-
-    max_val = max(max(x), max(y), limit)
-    extent = (0, max_val, 0, max_val)
-
-    ax.hexbin(x, y, C=count, cmap=cmap, norm=matplotlib.colors.LogNorm(),
-              gridsize=50, extent=extent, edgecolors='none', zorder=1)
-
-    if outliers_df is not None:
-        x, y = outliers_df[cols[0]], outliers_df[cols[1]]
-        if len(x) > 0:
-            ax.scatter(x, y, s=15, marker="x", facecolors="r",
-                       edgecolors='none', alpha=.3, zorder=3)
-
-    ax.set_xlim(0., max_val + .02)
-    ax.set_ylim(0., max_val + .02)
-    ax.set_aspect('equal')
-    ax.set_xlabel(cols[0], fontsize=fonts.ax_label_fontsize)
-    ax.set_ylabel(cols[1], fontsize=fonts.ax_label_fontsize)
-    ax.set_title(title, fontsize=fonts.title_fontsize)
-
-
-def plot_tc_hists(counts_df, ax, cols=[], title="", colors=[], thresholds=[]):
-    ax.set_yscale("log")
-    count = counts_df["Count"]
-    ref_line_colors = ["royalblue", "forestgreen"]
-    for i, col in enumerate(cols):
-        name = col.split(" ")[0]
-        tcs = counts_df[col]
-        alpha = 1.
-        if i > 0:
-            alpha = .9
-        else:
-            alpha = 1.
-        try:
-            color = colors[i]
-        except IndexError:
-            color = None
-        sns.distplot(tcs, label=name, kde=False, ax=ax, norm_hist=True,
-                     color=color, hist_kws={"linewidth": 0,
-                                            "histtype": "stepfilled",
-                                            "alpha": alpha,
-                                            "zorder": 2 * i + 1,
-                                            "weights": count})
-        ax.axvline(thresholds[i], linewidth=1, color=ref_line_colors[i],
-                   alpha=.75, linestyle='--', zorder=2 * i + 2)
-
-    ax.set_xlabel("TC", fontsize=fonts.ax_label_fontsize)
-    ax.set_ylabel("Log Frequency", fontsize=fonts.ax_label_fontsize)
-    ax.set_xlim(0, counts_df[cols].values.max())
-    ax.set_title(title, fontsize=fonts.title_fontsize)
-    ax.legend(loc=1, fontsize=fonts.legend_fontsize)
 
 
 def get_best_fold_by_prc(cv_dir):
@@ -190,7 +91,8 @@ if __name__ == "__main__":
     tc_examples_df = pd.DataFrame.from_csv(EXAMPLES_FILE, sep="\t")
     ax = plt.subplot(gs[0, :2])
     plot_tc_heatmap(tc_counts_df, ax=ax, outliers_df=None,
-                    cols=["ECFP4 TC", "E3FP Max TC"], cutoffs=[.3, .18])
+                    cols=["ECFP4 TC", "E3FP Max TC"],
+                    thresholds=[ECFP_TC_THRESH, E3FP_TC_THRESH])
     plot_tc_scatter(tc_examples_df, ax)
     sns.despine(ax=ax, offset=10)
     panel_axes.append(ax)
